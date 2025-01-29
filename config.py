33631204e404ca8dc85f5a7dfa6326943a599d55
SHAHIL440 <110395532+Shahilali5@users.noreply.github.com>
2025-01-29 09:56:32 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-# TEAM PURVI ALL COPYRIGHT ©️
+# TEAM SHAHIL COPYRIGHT ©️
 from pyrogram import filters
 import os
 
 class Config:
-    API_ID = "27079591"
-    API_HASH = "c81ae4c3dc026ea4bf49842a8ce4a5f9"
+    API_ID = "29727048"
+    API_HASH = "38d104adbd94c66a349714abd7977d80"
     #TOKEN = "6521122303:AAGCO3XMjcA0SN5NAi1M0NpmbmMxEtwwYbg"
     TOKEN = os.environ.get("TOKEN", None)
-    MONGO_URL = "mongodb+srv://vivek:1234567890@cluster0.c48d8ih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-    START_PIC = "https://files.catbox.moe/ppvvg0.jpg"
-    SUDOERS = filters.user(["6730956183"])
+    MONGO_URL = "mongodb+srv://Chatgpt:Chatgpt%40123@chatgpt.56l90.mongodb.net/?retryWrites=true&w=majority&appName=Chatgpt"
+    START_PIC = "https://envs.sh/aKf.mp4"
+    SUDOERS = filters.user(["8129810243"])
